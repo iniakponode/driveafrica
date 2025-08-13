092cb6113fa9d13d679e3520a5369cf6b1b0936b
Iniks <105390031+iniakponode@users.noreply.github.com>
2025-08-13 14:54:07 +0100

    Use route property for top-level navigation

--- app/src/main/java/com/uoa/safedriveafrica/presentation/daappnavigation/TopNavDestinations.kt
+++ app/src/main/java/com/uoa/safedriveafrica/presentation/daappnavigation/TopNavDestinations.kt
@@ -1,25 +1,32 @@
 package com.uoa.safedriveafrica.presentation.daappnavigation
 
 import com.uoa.safedriveafrica.R
+import com.uoa.core.utils.FILTER_SCREEN_ROUTE
+import com.uoa.core.utils.HOME_SCREEN_ROUTE
+import com.uoa.core.utils.SENSOR_CONTROL_SCREEN_ROUTE
 
 // Define the top level destinations for the app
 enum class TopLevelDestinations(
+    val route: String,
     val selectedIcon: Int,
     val unselectedIconResId: Int, // Store the resource ID here
     val titleTextId: Int,
 ) {
     HOME(
+        route = HOME_SCREEN_ROUTE,
         selectedIcon = R.drawable.home,
         unselectedIconResId = R.drawable.home, // Replace with your actual unselected home icon resource
         titleTextId = R.string.home,
     ),
     REPORTS(
+        route = FILTER_SCREEN_ROUTE,
         selectedIcon = R.drawable.report, // Replace with the correct selected icon if needed
         unselectedIconResId = R.drawable.history, // Replace with your actual report icon resource
         titleTextId = R.string.reports,
     ),
     RECORD_TRIP(
-        selectedIcon =R.drawable.tips, // Replace with the correct selected icon if needed
+        route = SENSOR_CONTROL_SCREEN_ROUTE,
+        selectedIcon = R.drawable.tips, // Replace with the correct selected icon if needed
         unselectedIconResId = R.drawable.tips, // Replace with your actual record trip icon resource
         titleTextId = R.string.record_trip,
     )
